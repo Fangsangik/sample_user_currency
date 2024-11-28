891524995fa8f4f2cacbc30cb20c89215ad9f3bc
Fangsangik <hwangsangik@gmail.com>
2024-11-28 19:43:31 +0900

    feat : ""

--- src/main/java/com/sparta/currency_user/CurrencyUserApplication.java
+++ src/main/java/com/sparta/currency_user/CurrencyUserApplication.java
@@ -2,8 +2,11 @@ package com.sparta.currency_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CurrencyUserApplication {
 
     public static void main(String[] args) {

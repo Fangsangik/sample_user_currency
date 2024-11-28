af9c5f8b302a175e4f3842d7f3cb95d2f35fe06c
Fangsangik <hwangsangik@gmail.com>
2024-11-28 19:30:41 +0900

    feat : "ExchangeResponseDto" - Dto

---
+++ src/main/java/com/sparta/currency_user/exchange/dto/ExchangeResponseDto.java
@@ -0,0 +1,34 @@
+package com.sparta.currency_user.exchange.dto;
+
+import com.sparta.currency_user.exchange.entity.Exchange;
+import com.sparta.currency_user.type.CurrencyName;
+import com.sparta.currency_user.type.ExchangeStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeResponseDto {
+    private String userName;
+    private CurrencyName currencyName;
+    private BigDecimal amountInKrw;
+    private BigDecimal amountAfterExchange;
+    private ExchangeStatus exchangeStatus;
+
+
+    public static ExchangeResponseDto fromEntity(Exchange exchange) {
+        return ExchangeResponseDto.builder()
+                .userName(exchange.getUser().getName())
+                .currencyName(exchange.getCurrency().getCurrencyName())
+                .amountInKrw(exchange.getAmountInKrw())
+                .amountAfterExchange(exchange.getAmountAfterExchange())
+                .exchangeStatus(exchange.getExchangeStatus())
+                .build();
+    }
+}

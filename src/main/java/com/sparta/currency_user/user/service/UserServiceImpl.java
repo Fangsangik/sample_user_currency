4050648293e2bd57eea4e1872c596438f48aa836
Fangsangik <hwangsangik@gmail.com>
2024-11-28 19:43:18 +0900

    feat : "UserServiceImpl" - 사용자 서비스 CRUD

---
+++ src/main/java/com/sparta/currency_user/user/service/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.sparta.currency_user.user.service;
+
+import com.sparta.currency_user.exception.CustomError;
+import com.sparta.currency_user.user.dto.UserRequestDto;
+import com.sparta.currency_user.user.dto.UserResponseDto;
+import com.sparta.currency_user.user.entity.User;
+import com.sparta.currency_user.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.sparta.currency_user.exception.type.ErrorType.*;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponseDto findById(Long id) {
+        return new UserResponseDto(findUserById(id));
+    }
+
+    @Transactional(readOnly = true)
+    public User findUserById(Long id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new CustomError(USER_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserResponseDto::toDto)
+                .toList();
+    }
+
+    @Transactional
+    public UserResponseDto save(UserRequestDto userRequestDto) {
+
+        User savedUser = userRepository.save(UserRequestDto.toEntity(userRequestDto));
+        return new UserResponseDto(savedUser);
+    }
+
+
+    @Transactional
+    public void deleteUserById(Long id) {
+        this.findUserById(id);
+        userRepository.deleteById(id);
+    }
+
+}

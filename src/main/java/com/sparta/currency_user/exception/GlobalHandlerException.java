424c0eb33c89c749b41b06ec0255aa6c9a8a1be8
Fangsangik <hwangsangik@gmail.com>
2024-11-28 19:36:48 +0900

    feat : "GlobalHandlerException" - GlobalHandlerException으로 전체 Error 처리

---
+++ src/main/java/com/sparta/currency_user/exception/GlobalHandlerException.java
@@ -0,0 +1,19 @@
+package com.sparta.currency_user.exception;
+
+import com.sparta.currency_user.exception.type.ErrorType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(CustomError.class)
+    public ResponseEntity<ErrorResponse> handleCustomError(CustomError error) {
+        ErrorType errorType = error.getErrorType();
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getMessage(), errorType.getStatus());
+        return ResponseEntity
+                .status(errorType.getStatus())
+                .body(response);
+    }
+}

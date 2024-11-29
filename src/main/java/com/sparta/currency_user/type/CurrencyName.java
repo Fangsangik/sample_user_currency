b48904a234c27e11d977cd0c55611587a1bab143
Fangsangik <hwangsangik@gmail.com>
2024-11-29 15:55:54 +0900

    refactor : " formatAmount" - 삭제

--- src/main/java/com/sparta/currency_user/type/CurrencyName.java
+++ src/main/java/com/sparta/currency_user/type/CurrencyName.java
@@ -21,14 +21,5 @@ public enum CurrencyName {
         this.wholeNumber = wholeNumber;
     }
 
-    public String formatAmount(BigDecimal amount) {
-        if (wholeNumber) {
-            // 소수점 이하를 버리도록 설정
-            amount = amount.setScale(0, RoundingMode.DOWN);
-        } else {
-            // 소수점 이하 2자리로 반올림 설정 (추가적으로 필요한 경우)
-            amount = amount.setScale(2, RoundingMode.HALF_UP);
-        }
-        return amount.toPlainString() + " " + symbol;
-    }
+
 }

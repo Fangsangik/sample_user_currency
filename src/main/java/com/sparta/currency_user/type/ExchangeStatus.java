5b640852457389f62d76fc86fe2512b87e4c8068
Fangsangik <hwangsangik@gmail.com>
2024-11-29 16:05:55 +0900

    refactor : "ExchangeStatus" - 오타 수정

--- src/main/java/com/sparta/currency_user/type/ExchangeStatus.java
+++ src/main/java/com/sparta/currency_user/type/ExchangeStatus.java
@@ -2,7 +2,7 @@ package com.sparta.currency_user.type;
 
 public enum ExchangeStatus {
     NORMAL("정상상태"),
-    CANCELLED("취소"),
+    CANCELED("취소"),
     PENDING("대기중");
 
     private String message;

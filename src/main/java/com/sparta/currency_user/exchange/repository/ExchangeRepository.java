2ba02c50c8f08b139e2dc94cb5e27576a87d6dc8
Fangsangik <hwangsangik@gmail.com>
2024-11-29 16:05:33 +0900

    feat : "ExchangeRepository" - Spring Data JPQ + JPQL을 사용해 Service 로직에서는 query 부분 작성 뺌

--- src/main/java/com/sparta/currency_user/exchange/repository/ExchangeRepository.java
+++ src/main/java/com/sparta/currency_user/exchange/repository/ExchangeRepository.java
@@ -1,8 +1,42 @@
 package com.sparta.currency_user.exchange.repository;
 
+import com.sparta.currency_user.exchange.dto.ExchangeSumDto;
 import com.sparta.currency_user.exchange.entity.Exchange;
+import com.sparta.currency_user.type.ExchangeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
 
+    List<Exchange> findByUserId(Long userId);
+
+    @Transactional
+    @Modifying
+    @Query("update Exchange e set e.exchangeStatus = :exchangeStatus where e.user.id = :userId and e.currency.id = :currencyId")
+    int updateExchangeStatus(@Param("exchangeStatus") ExchangeStatus exchangeStatus,
+                             @Param("userId") Long userId,
+                             @Param("currencyId") Long currencyId);
+
+
+    @Query("SELECT new com.sparta.currency_user.exchange.dto.ExchangeSumDto(COUNT(e), SUM(e.amountInKrw)) " +
+            "FROM Exchange e " +
+            "WHERE e.user.id = :userId " +
+            "GROUP BY e.user.id")
+    ExchangeSumDto calculateExchangeSummary(@Param("userId") Long userId);
+
+    List<Exchange> findByUserIdAndCurrencyId(Long userId, Long currencyId);
+
+    @Transactional
+    @Modifying
+    @Query("update Exchange e set e.amountAfterExchange = :amountAfterExchange," +
+            "e.exchangeStatus = :exchangeStatus where e.id = :exchangeId")
+    int updateExchangeAmountAndStatus(@Param("exchangeId") Long exchangeId,
+                                      @Param("amountAfterExchange")BigDecimal amountAfterExchange,
+                                      @Param("exchangeStatus") ExchangeStatus exchangeStatus);
 }
